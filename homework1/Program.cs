dc86ee8cb43497dccb096dbc3ee913e813599406
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-10-16 18:34:52 +0300

    task2

--- homework1/Program.cs
+++ homework1/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.//
-
+/*
 Console.Write("Input a first number: ");
 int n1=Convert.ToInt32(Console.ReadLine());
 Console.Write("Input a second number: ");
@@ -18,3 +18,23 @@ else
     min=n1;
 }
 Console.WriteLine($"Number {max} is the biigest and number {min} is the smallest");
+*/
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.//
+
+Console.Write("Input a first number: ");
+int n1=Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2=Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a third number: ");
+int n3=Convert.ToInt32(Console.ReadLine());
+
+int max=n1;
+
+if (n2>max) max=n2;
+if (n3>max) max=n3;
+
+Console.Write("max=");
+Console.WriteLine(max);
+
+
