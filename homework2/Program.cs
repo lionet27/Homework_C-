e76627584216449e53c1e8c5e4b0b7b8b107c96a
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-10-19 19:52:39 +0300

    add task10

---
+++ homework2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+int SecondDigit(int num)
+{
+    int des=num/10;
+    int secnumer=des%10;
+    return secnumer;
+}
+
+Console.Write("Input a triple digits:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Second digit of {num} is {SecondDigit(num)}");
+
