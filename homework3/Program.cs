cb50a1d571070a2ca31aebdd9995db5524942c40
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-10-21 02:50:32 +0300

    add task19

---
+++ homework3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+void Palindrom(string num)
+{
+    string? numRev=new string(num.ToCharArray().Reverse().ToArray());
+    if (num==numRev)
+        Console.WriteLine("It is palindrome");
+    else
+    {
+        Console.WriteLine("It is not palindrome");
+    }
+}
+
+Console.Write("Input a number:");
+string? number = Console.ReadLine();
+Palindrom(number);
+
