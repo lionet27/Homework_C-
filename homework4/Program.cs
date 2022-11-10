20e4494f902854a0e1cda5108ba3af3ce5e5f83d
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-11-10 21:48:55 +0300

    add homework4

---
+++ homework4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int Stepen(int a,int b)
+{
+    int step=1;
+    int index=1;
+    while(index<=b)
+        {
+            step=step*a;
+            index++;
+
+        }
+     return step;
+}
+
+Console.WriteLine("input A:");
+int A=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input B:");
+int B=Convert.ToInt32(Console.ReadLine());
+if (B<0) 
+    {
+        Console.WriteLine("Пожалуйста, введите положительное число B.");
+    }
+
+    else
+    {
+        Console.WriteLine($"{A} в степени {B}:{Stepen(A,B)}");
+    }
