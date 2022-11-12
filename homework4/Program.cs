196e5a6c9cee1d9311f573ced8a8c17cecdebc63
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-11-12 17:38:13 +0300

    add task 29

--- homework4/Program.cs
+++ homework4/Program.cs
@@ -29,6 +29,7 @@ if (B<0)
 */
 
 //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*
 int SumDigit(int num)
 {
     if (num<0) num=num*-1;
@@ -46,3 +47,39 @@ int SumDigit(int num)
 Console.WriteLine("input a number:");
 int number=Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Сумма цифр в числе {number} равняется:{SumDigit(number)}");
+*/
+
+// Задача 29: Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int[] FillArray(int size)
+{
+    int[] array=new int[size];
+    for(int i=0;i<size;i++)
+    {
+        Console.WriteLine("Input element of array:");
+        array[i]=Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void ShowArray(int[]array)
+{
+        Console.Write("Array is:[");
+
+    for(int i=0; i<(array.Length-1); i++)
+    {
+        Console.Write(array[i] + " ");
+        Console.Write(",");
+    }
+    //Console.Write("Q"+ "]");
+}
+
+Console.WriteLine("Input number of elements: ");
+int n=Convert.ToInt32(Console.ReadLine());
+
+int[] myArray=FillArray(n);
+ShowArray(myArray);
+
