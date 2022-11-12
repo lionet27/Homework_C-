50aea2e98e0ec7bf65abaa85f9d35900d59fce21
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-11-12 11:35:54 +0300

    add task27

--- homework4/Program.cs
+++ homework4/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*
 int Stepen(int a,int b)
 {
     int step=1;
@@ -25,3 +26,23 @@ if (B<0)
     {
         Console.WriteLine($"{A} в степени {B}:{Stepen(A,B)}");
     }
+*/
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumDigit(int num)
+{
+    if (num<0) num=num*-1;
+    
+    int sum=0;
+    int current=num;
+    while(current>0)
+        {
+            sum=sum+current%10;
+            current=current/10;
+        }
+     return sum;
+}
+
+Console.WriteLine("input a number:");
+int number=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {number} равняется:{SumDigit(number)}");
