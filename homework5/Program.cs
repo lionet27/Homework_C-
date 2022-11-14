a4fc8b1dae0074b9842e432d367894469945de2e
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-11-14 19:52:45 +0300

    add task38

--- homework5/Program.cs
+++ homework5/Program.cs
@@ -46,6 +46,7 @@ Console.WriteLine($"Amount even number in array is {AmountEvenNumber(myArray)}")
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+/*
 int SumOddIndex(int[] array)
 {
     int sum=0;
@@ -66,3 +67,55 @@ int[] myArray=CreateRandomArray(n,min,max);
 ShowArray(myArray);
 
 Console.WriteLine($"Amount numbers with odd index in array is {SumOddIndex(myArray)}");
+*/
+
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+double[] CreateRandomDoubleArray(int size,int minValue,int maxValue)
+{
+    double[] array=new double[size];
+    for(int i=0;i<size;i++)
+        array[i]=Math.Round((new Random().Next(minValue, maxValue+1)+new Random().NextDouble()),2);
+    
+    return array;
+}
+
+void ShowArrayDouble(double[]array)
+{
+        Console.Write("Array is:[");
+
+    for(int i=0; i<(array.Length-1); i++)
+    {
+        Console.Write(array[i] + " ");
+        Console.Write(",");
+    }
+    Console.WriteLine(array[array.Length-1]+ "]");
+}
+
+double DifferenceMaxMin(double[]array)
+{
+    double min=array[0];
+    double max=array[0];
+    for(int i=0;i<array.Length;i++)
+        if(array[i]>max)
+            max=array[i];
+        else
+        {
+            if(array[i]<min)
+                min=array[i];
+        }
+    double difference=Math.Round((max-min),2);
+    return difference;
+}
+
+Console.WriteLine("Input number of elements: ");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a min possible value: ");
+int min=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a max possible value: ");
+int max=Convert.ToInt32(Console.ReadLine());
+
+double[] myArray=CreateRandomDoubleArray(n,min,max);
+ShowArrayDouble(myArray);
+
+Console.WriteLine($"Difference between max and min value in array is {DifferenceMaxMin(myArray)}");
