1e816d63615011cd992d4cd966d061127091d8d9
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-11-16 21:56:11 +0300

    add task43

--- homework6/Program.cs
+++ homework6/Program.cs
@@ -1,25 +1,6 @@
-﻿int[] CreateRandomArray(int size,int minValue,int maxValue)
-{
-    int[] array=new int[size];
-    for(int i=0;i<size;i++)
-        array[i]=new Random().Next(minValue, maxValue+1);
-    
-    return array;
-}
-
-void ShowArray(int[]array)
-{
-        Console.Write("Array is:[");
-
-    for(int i=0; i<(array.Length-1); i++)
-    {
-        Console.Write(array[i] + " ");
-        Console.Write(",");
-    }
-    Console.WriteLine(array[array.Length-1]+ "]");
-}
-
+﻿
 //Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
 
 int QuantityPositive(int m)
 {
@@ -37,7 +18,33 @@ Console.WriteLine("Input number of elements: ");
 int n=Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Number of pozitive elements is "+QuantityPositive(n));
 
-
+*/
 
 //Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Input b1: ");
+double b1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input k1: ");
+double k1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input b2: ");
+double b2=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Input k2: ");
+double k2=Convert.ToDouble(Console.ReadLine());
+
+if(k1==k2)
+    {
+        if(b1==b2)
+            Console.WriteLine("Прямые совпадают");
+        else
+            Console.WriteLine("Прямые параллельные");
+    }
+else 
+    {
+        double x=Math.Round(((b2-b1)/(k1-k2)),2);
+        double y=k1*x+b1;
+
+        Console.WriteLine($"Intersection point of two lines is x={x},y={y} ");
+    }
+
