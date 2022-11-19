d53b8ee9d4b54a812f3cfbded6f201f53daba49b
«OlgaSoboleva27» <osoboleva27@gmail.com>
2022-11-19 23:36:32 +0300

    add task 50

--- homework7/Program.cs
+++ homework7/Program.cs
@@ -1,5 +1,5 @@
 ﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
-
+/*
 double[,] CreateRandom2dDoubleArray(int rows,int columns, int minValue,int maxValue)
 
 {
@@ -37,7 +37,7 @@ int max=Convert.ToInt32(Console.ReadLine());
 
 double[,] myArray=CreateRandom2dDoubleArray(m,n,min,max);
 Show2dDoubleArray(myArray);
-
+*/
 //Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
 
@@ -65,3 +65,61 @@ void Show2dArray(int[,]array)
     
     Console.WriteLine();
 }
+
+Console.WriteLine("Input number of  rows: ");
+int m=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input number of columns: ");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a min possible value: ");
+int min=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a max possible value: ");
+int max=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input idex of  row: ");
+int i=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input index of column: ");
+int j=Convert.ToInt32(Console.ReadLine());
+
+// Решение через 2 метода. Проверки наличия элемента и вывода его значения при наличии. 
+
+/*
+bool IsThereElement(int[,]array,int i,int j)
+
+{
+    if (i<(array.GetLength(0))&&j<(array.GetLength(1)))
+        return true;
+    else
+        return false;
+}
+
+int ElementIJ(int[,]array,int i,int j)
+{
+    int element=array[i,j];
+    return element;
+}
+
+int[,] myArray=CreateRandom2dArray(m,n,min,max);
+Show2dArray(myArray);
+Console.WriteLine($"There is Element[{i},{j}] in array.It is "+IsThereElement(myArray,i,j));
+if (IsThereElement(myArray,i,j)==true)
+    {
+        int elementij=ElementIJ(myArray,i,j);
+        Console.WriteLine($"Element[{i},{j}]={elementij}");
+    }
+
+*/
+//Решение через void метод.
+
+void ShowElementIJ(int[,]array, int i, int j)
+
+{
+    if (i<(array.GetLength(0))&&j<(array.GetLength(1)))
+        Console.WriteLine($"Element[{i},{j}]={array[i,j]}");
+    else
+        Console.WriteLine($"There is no element[{i},{j}].");
+}
+
+int[,] myArray=CreateRandom2dArray(m,n,min,max);
+Show2dArray(myArray);
+ShowElementIJ(myArray,i,j);
+
